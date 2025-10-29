0847fc8ff1917c343fea5df331fae2d3255f0f4d
Oleksandr Sapozhnikov <a.d.sapozhnikov@gmail.com>
2025-10-29 16:50:14 +0200

    [OSAPO] Add outputs

---
+++ tests/outputs.tf
@@ -0,0 +1,19 @@
+output "component_id" {
+  description = "ID of the created component"
+  value       = compass_component.example.id
+}
+
+output "component_name" {
+  description = "Name of the created component"
+  value       = compass_component.example.name
+}
+
+output "component_description" {
+  description = "Description of the created component"
+  value       = compass_component.example.description
+}
+
+output "component_type" {
+  description = "Type of the created component"
+  value       = compass_component.example.type
+}
